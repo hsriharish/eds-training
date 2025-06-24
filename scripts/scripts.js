096fb174946e7d15e1cc79fa34ef8ebe9ed9161b
Nasaka <hsriharish@deloitte.com>
2025-06-24 11:14:30 +0530

    adding scripts file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,6 +67,13 @@ export function decorateMain(main) {
   decorateBlocks(main);
 }
 
+export function getHref() {
+  if (window.location.href !== 'about:srcdoc') return window.location.href;
+  const { location: parentLocation } = window.parent;
+  const urlParams = new URLSearchParams(parentLocation.search);
+  return `${parentLocation.origin}${urlParams.get('path')}`;
+}
+
 /**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
@@ -79,6 +86,12 @@ async function loadEager(doc) {
     decorateMain(main);
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
+  }
+
+  const path = getHref();
+  if (path.includes('/ar/')) {
+    document.documentElement.lang = 'ar';
+    document.documentElement.dir = 'rtl';
   }
 
   try {
